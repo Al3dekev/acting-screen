a46d7686895ba953a0f02437263d3bd2824a3867
Al3dekev <quentin1998@me.com>
2018-12-05 16:47:09 +0100

    Initial commit

---
+++ src/main/java/com/github/al3dekev/actingscreen/ActingScreenApplication.java
@@ -0,0 +1,16 @@
+package com.github.al3dekev.actingscreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActingScreenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActingScreenApplication.class, args);
+
+
+        
+
+    }
+}
